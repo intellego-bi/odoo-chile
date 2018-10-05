6c5bbaeee38bade9ae584745a250b78cf8672632
Rodolfo Bermúdez-Neubauer <43762610+intellego-rbn@users.noreply.github.com>
2018-10-04 23:32:56 -0300

    Add files via upload
    l10n_cl_chart_of_account

---
+++ l10n_cl_chart_of_account/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (c) 2018 Konos Soluciones y Servicios Limitada http://www.konos.cl
+
+{
+    'name': 'Chile Localization Chart Account SII',
+    'version': '1.7',
+    'description': """
+Chilean accounting chart and tax localization.
+==============================================
+Plan contable chileno e impuestos de acuerdo a disposiciones vigentes,
+basado en plan de cuentas del Servicio de Impuestos Internos
+
+    """,
+    'author': 'Konos',
+    'website': 'http://www.konos.cl',
+    'category': 'Localization/Chile',    
+    'license': 'AGPL-3',
+    'depends': ['account'],
+    'data': [
+            'views/account_tax.xml',
+            'data/l10n_cl_chart_of_account_data.xml',
+            'data/account_tax_data.xml',
+            'data/account_chart_template_data.yml',
+            'data/account_journal.xml',
+    ],
+}
